bc667c2a1095b2e1c9c683debce6901879500b10
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-04-24 15:25:04 -0700

    test without plugin

--- recOrder/tests/widget_tests/test_dock_widget.py
+++ recOrder/tests/widget_tests/test_dock_widget.py
@@ -1,9 +1,6 @@
 from recOrder.plugin.main_widget import MainWidget
-# import napari
-from napari.viewer import ViewerModel
 
 
 def test_dock_widget(make_napari_viewer):
-    viewer: ViewerModel = make_napari_viewer()
-    viewer.window.add_dock_widget(MainWidget(viewer))
-    assert "recOrder" in list(viewer._window._dock_widgets.keys())[0]
+    viewer = make_napari_viewer()
+    assert viewer
