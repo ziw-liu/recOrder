fbbdd974fec0b8f63883b24f2c676e2c389d647d
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-05-03 14:55:55 -0700

    print display env

--- recOrder/tests/widget_tests/test_dock_widget.py
+++ recOrder/tests/widget_tests/test_dock_widget.py
@@ -1,3 +1,10 @@
+import os
+import warnings
+
+
 def test_dock_widget(make_napari_viewer):
+    display = os.environ.get("DISPLAY")
+    if display:
+        warnings.warn(f"DISPLAY: {display}")
     viewer = make_napari_viewer()
     assert viewer
